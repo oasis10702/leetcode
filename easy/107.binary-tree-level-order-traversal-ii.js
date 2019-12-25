9b826e17ce7f2cbcac01cb298a272c92d3d0c837
Ho_Weihong <weihong_ho@haraguchi-10d1.Area51.DigitalGroup.local>
2019-12-25 12:20:50 +0900

    add problem107

---
+++ easy/107.binary-tree-level-order-traversal-ii.js
@@ -0,0 +1,73 @@
+/*
+ * @lc app=leetcode id=107 lang=javascript
+ *
+ * [107] Binary Tree Level Order Traversal II
+ *
+ * https://leetcode.com/problems/binary-tree-level-order-traversal-ii/description/
+ *
+ * algorithms
+ * Easy (49.16%)
+ * Likes:    939
+ * Dislikes: 177
+ * Total Accepted:    265.3K
+ * Total Submissions: 539K
+ * Testcase Example:  '[3,9,20,null,null,15,7]'
+ *
+ * Given a binary tree, return the bottom-up level order traversal of its
+ * nodes' values. (ie, from left to right, level by level from leaf to root).
+ *
+ *
+ * For example:
+ * Given binary tree [3,9,20,null,null,15,7],
+ *
+ * ⁠   3
+ * ⁠  / \
+ * ⁠ 9  20
+ * ⁠   /  \
+ * ⁠  15   7
+ *
+ *
+ *
+ * return its bottom-up level order traversal as:
+ *
+ * [
+ * ⁠ [15,7],
+ * ⁠ [9,20],
+ * ⁠ [3]
+ * ]
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number[][]}
+ */
+var levelOrderBottom = function(root) {
+  const arr = [];
+  dfs(root, 0);
+
+  function dfs(node, lv) {
+    if (!node) return;
+
+    if (!arr[lv]) {
+      arr[lv] = [node.val];
+    } else {
+      arr[lv].push(node.val);
+    }
+
+    dfs(node.left, lv + 1);
+    dfs(node.right, lv + 1);
+  }
+
+  return arr.reverse();
+};
+// @lc code=end
