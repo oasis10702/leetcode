bd3d0eb4ea6eaeff5632c8ddfe803f2dccf38fbb
oasis10702 <oasis10702@gmail.com>
2020-01-09 13:07:34 +0900

    add problem15

---
+++ medium/15.3-sum.js
@@ -0,0 +1,69 @@
+/*
+ * @lc app=leetcode id=15 lang=javascript
+ *
+ * [15] 3Sum
+ *
+ * https://leetcode.com/problems/3sum/description/
+ *
+ * algorithms
+ * Medium (25.26%)
+ * Likes:    5021
+ * Dislikes: 610
+ * Total Accepted:    717.5K
+ * Total Submissions: 2.8M
+ * Testcase Example:  '[-1,0,1,2,-1,-4]'
+ *
+ * Given an array nums of n integers, are there elements a, b, c in nums such
+ * that a + b + c = 0? Find all unique triplets in the array which gives the
+ * sum of zero.
+ *
+ * Note:
+ *
+ * The solution set must not contain duplicate triplets.
+ *
+ * Example:
+ *
+ *
+ * Given array nums = [-1, 0, 1, 2, -1, -4],
+ *
+ * A solution set is:
+ * [
+ * ⁠ [-1, 0, 1],
+ * ⁠ [-1, -1, 2]
+ * ]
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+  let ans = [];
+  if (nums.length < 3) return ans;
+  nums.sort((a, b) => a - b);
+  for (let i = 0; i < nums.length - 2; i++) {
+    if (nums[i] > 0) break;
+    if (i > 0 && nums[i] === nums[i - 1]) continue;
+    let L = i + 1;
+    let R = nums.length - 1;
+    while (L < R) {
+      const sum = nums[i] + nums[L] + nums[R];
+      if (sum === 0) {
+        ans.push([nums[i], nums[L], nums[R]]);
+        while (L < R && nums[L] === nums[L + 1]) L++;
+        while (L < R && nums[R] === nums[R - 1]) R--;
+        L++;
+        R--;
+      } else if (sum < 0) {
+        L++;
+      } else if (sum > 0) {
+        R--;
+      }
+    }
+  }
+
+  return ans;
+};
+// @lc code=end
