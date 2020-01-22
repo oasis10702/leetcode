1dcd5d281b09e34492d4132c6f817c540bbf6a65
oasis10702 <oasis10702@gmail.com>
2020-01-22 16:20:16 +0900

    add problem151

---
+++ medium/151.reverse-words-in-a-string.js
@@ -0,0 +1,76 @@
+/*
+ * @lc app=leetcode id=151 lang=javascript
+ *
+ * [151] Reverse Words in a String
+ *
+ * https://leetcode.com/problems/reverse-words-in-a-string/description/
+ *
+ * algorithms
+ * Medium (18.68%)
+ * Likes:    775
+ * Dislikes: 2582
+ * Total Accepted:    344.7K
+ * Total Submissions: 1.8M
+ * Testcase Example:  '"the sky is blue"'
+ *
+ * Given an input string, reverse the string word by word.
+ *
+ *
+ *
+ * Example 1:
+ *
+ *
+ * Input: "the sky is blue"
+ * Output: "blue is sky the"
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: "  hello world!  "
+ * Output: "world! hello"
+ * Explanation: Your reversed string should not contain leading or trailing
+ * spaces.
+ *
+ *
+ * Example 3:
+ *
+ *
+ * Input: "a good   example"
+ * Output: "example good a"
+ * Explanation: You need to reduce multiple spaces between two words to a
+ * single space in the reversed string.
+ *
+ *
+ *
+ *
+ * Note:
+ *
+ *
+ * A word is defined as a sequence of non-space characters.
+ * Input string may contain leading or trailing spaces. However, your reversed
+ * string should not contain leading or trailing spaces.
+ * You need to reduce multiple spaces between two words to a single space in
+ * the reversed string.
+ *
+ *
+ *
+ *
+ * Follow up:
+ *
+ * For C programmers, try to solve it in-place in O(1) extra space.
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var reverseWords = function(s) {
+  return s
+    .trim()
+    .split(/\s+/)
+    .reverse()
+    .join(' ');
+};
+// @lc code=end
