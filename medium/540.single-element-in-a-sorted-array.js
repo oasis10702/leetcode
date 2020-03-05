54ea9653ff9ca65ec6403c41c0347b89ca2bd734
oasis10702 <oasis10702@gmail.com>
2020-03-05 22:57:13 +0900

    update problem540

--- medium/540.single-element-in-a-sorted-array.js
+++ medium/540.single-element-in-a-sorted-array.js
@@ -2,41 +2,6 @@
  * @lc app=leetcode id=540 lang=javascript
  *
  * [540] Single Element in a Sorted Array
- *
- * https://leetcode.com/problems/single-element-in-a-sorted-array/description/
- *
- * algorithms
- * Medium (57.55%)
- * Likes:    1042
- * Dislikes: 72
- * Total Accepted:    77K
- * Total Submissions: 133.6K
- * Testcase Example:  '[1,1,2,3,3,4,4,8,8]'
- *
- * You are given a sorted array consisting of only integers where every element
- * appears exactly twice, except for one element which appears exactly once.
- * Find this single element that appears only once.
- *
- *
- *
- * Example 1:
- *
- *
- * Input: [1,1,2,3,3,4,4,8,8]
- * Output: 2
- *
- *
- * Example 2:
- *
- *
- * Input: [3,3,7,7,10,11,11]
- * Output: 10
- *
- *
- *
- *
- * Note: Your solution should run in O(log n) time and O(1) space.
- *
  */
 
 // @lc code=start
@@ -45,28 +10,37 @@
  * @return {number}
  */
 var singleNonDuplicate = function(nums) {
+  /* O(n)
+  let p = 0;
+  while (p <= nums.length - 1) {
+    if (nums[p] !== nums[p + 1]) return nums[p];
+    p += 2;
+  }
+  */
   let lo = 0;
   let hi = nums.length - 1;
-
-  //   while (lo < hi) {
-  //     const mid = Math.floor((lo + hi) / 2);
-  //     const isEven = (hi - mid) % 2 === 0;
-  //     if (nums[mid + 1] === nums[mid]) {
-  //       if (isEven) {
-  //         lo = mid + 2;
-  //       } else {
-  //         hi = mid - 1;
-  //       }
-  //     } else if (nums[mid - 1] === nums[mid]) {
-  //       if (isEven) {
-  //         hi = mid - 2;
-  //       } else {
-  //         lo = mid + 1;
-  //       }
-  //     } else {
-  //       return nums[mid];
-  //     }
-  //   }
+  /* two pointer #1
+    while (lo < hi) {
+      const mid = Math.floor((lo + hi) / 2);
+      const isEven = (hi - mid) % 2 === 0;
+      if (nums[mid + 1] === nums[mid]) {
+        if (isEven) {
+          lo = mid + 2;
+        } else {
+          hi = mid - 1;
+        }
+      } else if (nums[mid - 1] === nums[mid]) {
+        if (isEven) {
+          hi = mid - 2;
+        } else {
+          lo = mid + 1;
+        }
+      } else {
+        return nums[mid];
+      }
+    }
+  */
+  // two pointer #2
   while (lo < hi) {
     let mid = Math.floor((lo + hi) / 2);
     if (mid % 2 === 1) mid--;
