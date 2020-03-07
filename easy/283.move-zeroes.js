b5c5fd18011ce03b3c90c8ed53297b814cf0182c
oasis10702 <oasis10702@gmail.com>
2020-03-07 23:33:19 +0900

    add problem90

---
+++ easy/283.move-zeroes.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=283 lang=javascript
+ *
+ * [283] Move Zeroes
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function(nums) {
+  // TODO
+};
+// @lc code=end
