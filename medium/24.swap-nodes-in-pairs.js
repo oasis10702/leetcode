1f35b8591a1a320b65392f7df27dbb8ad3642bdf
oasis10702 <oasis10702@gmail.com>
2020-03-17 18:45:29 +0900

    add problem24

---
+++ medium/24.swap-nodes-in-pairs.js
@@ -0,0 +1,53 @@
+/*
+ * @lc app=leetcode id=24 lang=javascript
+ *
+ * [24] Swap Nodes in Pairs
+ *
+ * https://leetcode.com/problems/swap-nodes-in-pairs/description/
+ *
+ * algorithms
+ * Medium (47.25%)
+ * Likes:    1583
+ * Dislikes: 137
+ * Total Accepted:    383.4K
+ * Total Submissions: 809.5K
+ * Testcase Example:  '[1,2,3,4]'
+ *
+ * Given a linked list, swap every two adjacent nodes and return its head.
+ *
+ * You may not modify the values in the list's nodes, only nodes itself may be
+ * changed.
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ * Given 1->2->3->4, you should return the list as 2->1->4->3.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val) {
+ *     this.val = val;
+ *     this.next = null;
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var swapPairs = function(head) {
+  if (!head) return head;
+
+  let prev = head;
+  let next = head.next;
+  // TODO
+  while (prev && prev.next) {}
+
+  return head;
+};
+// @lc code=end
