2f883c0d6da1c61c4a57c3d5dfbc7046acf93265
oasis10702 <oasis10702@gmail.com>
2020-03-18 16:27:06 +0900

    add problem34

---
+++ medium/34.find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,60 @@
+/*
+ * @lc app=leetcode id=34 lang=javascript
+ *
+ * [34] Find First and Last Position of Element in Sorted Array
+ *
+ * https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/description/
+ *
+ * algorithms
+ * Medium (34.63%)
+ * Likes:    2268
+ * Dislikes: 109
+ * Total Accepted:    387.2K
+ * Total Submissions: 1.1M
+ * Testcase Example:  '[5,7,7,8,8,10]\n8'
+ *
+ * Given an array of integers nums sorted in ascending order, find the starting
+ * and ending position of a given target value.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * If the target is not found in the array, return [-1, -1].
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 8
+ * Output: [3,4]
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [5,7,7,8,8,10], target = 6
+ * Output: [-1,-1]
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var searchRange = function(nums, target) {
+  /* O(n) solution
+  let first = nums.indexOf(target);
+  let last = first;
+  if (first === -1) {
+    return [-1, -1];
+  }
+
+  for (let i = first + 1; i < nums.length; i++) {
+    if (nums[i] === target) {
+      last = i;
+    }
+  }
+
+  return [first, last];
+  */
+};
+// @lc code=end
