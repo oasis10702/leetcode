2a94b5117c96dcabe35ade8e8294485f5048c3f1
oasis10702 <oasis10702@gmail.com>
2020-03-20 23:24:57 +0900

    add problem645

---
+++ easy/645.set-mismatch.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode id=645 lang=javascript
+ *
+ * [645] Set Mismatch
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var findErrorNums = function(nums) {
+  const tb = {};
+  for (let i = 0; i < nums.length; i++) {
+    // TODO
+  }
+};
+// @lc code=end
