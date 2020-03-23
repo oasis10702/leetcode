fd56f794d2142ec9c35ae222811d197f9d0fdeeb
oasis10702 <oasis10702@gmail.com>
2020-03-23 15:25:12 +0900

    add problem33

---
+++ medium/33.search-in-rotated-sorted-array.js
@@ -0,0 +1,89 @@
+/*
+ * @lc app=leetcode id=33 lang=javascript
+ *
+ * [33] Search in Rotated Sorted Array
+ *
+ * https://leetcode.com/problems/search-in-rotated-sorted-array/description/
+ *
+ * algorithms
+ * Medium (33.41%)
+ * Likes:    3343
+ * Dislikes: 378
+ * Total Accepted:    529K
+ * Total Submissions: 1.6M
+ * Testcase Example:  '[4,5,6,7,0,1,2]\n0'
+ *
+ * Suppose an array sorted in ascending order is rotated at some pivot unknown
+ * to you beforehand.
+ *
+ * (i.e., [0,1,2,4,5,6,7] might become [4,5,6,7,0,1,2]).
+ *
+ * You are given a target value to search. If found in the array return its
+ * index, otherwise return -1.
+ *
+ * You may assume no duplicate exists in the array.
+ *
+ * Your algorithm's runtime complexity must be in the order of O(log n).
+ *
+ * Example 1:
+ *
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 0
+ * Output: 4
+ *
+ *
+ * Example 2:
+ *
+ *
+ * Input: nums = [4,5,6,7,0,1,2], target = 3
+ * Output: -1
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let left = 0;
+  let right = nums.length - 1;
+
+  // find rotate point
+  while (left < right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] > nums[right]) {
+      left = mid + 1;
+    } else {
+      right = mid;
+    }
+  }
+
+  const rotate = left;
+  left = 0;
+  right = nums.length - 1;
+
+  if (target >= nums[rotate] && target <= nums[right]) {
+    left = rotate;
+  } else {
+    right = rotate - 1;
+  }
+
+  while (left <= right) {
+    const mid = Math.floor((left + right) / 2);
+
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+
+  return -1;
+};
+// @lc code=end
