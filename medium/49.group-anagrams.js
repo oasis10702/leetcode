4aa1776c4da12d872d94034c3155b6022da19b48
oasis10702 <oasis10702@gmail.com>
2020-03-23 11:55:01 +0900

    add problem49

---
+++ medium/49.group-anagrams.js
@@ -0,0 +1,66 @@
+/*
+ * @lc app=leetcode id=49 lang=javascript
+ *
+ * [49] Group Anagrams
+ *
+ * https://leetcode.com/problems/group-anagrams/description/
+ *
+ * algorithms
+ * Medium (51.24%)
+ * Likes:    2667
+ * Dislikes: 154
+ * Total Accepted:    508.1K
+ * Total Submissions: 956.4K
+ * Testcase Example:  '["eat","tea","tan","ate","nat","bat"]'
+ *
+ * Given an array of strings, group anagrams together.
+ *
+ * Example:
+ *
+ *
+ * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+ * Output:
+ * [
+ * ⁠ ["ate","eat","tea"],
+ * ⁠ ["nat","tan"],
+ * ⁠ ["bat"]
+ * ]
+ *
+ * Note:
+ *
+ *
+ * All inputs will be in lowercase.
+ * The order of your output does not matter.
+ *
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string[][]}
+ */
+var groupAnagrams = function(strs) {
+  const tb = {};
+  const ans = [];
+  let index = 0;
+
+  for (let str of strs) {
+    const sortStr = str
+      .split('')
+      .sort()
+      .join('');
+
+    if (!tb[sortStr] && tb[sortStr] !== 0) {
+      tb[sortStr] = index;
+      ans[index] = [str];
+      index++;
+    } else {
+      ans[tb[sortStr]].push(str);
+    }
+  }
+
+  return ans;
+};
+
+// @lc code=end
