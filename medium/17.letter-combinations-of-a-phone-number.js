ff5be4e5520771510d4155900bc2d50888fd2945
oasis10702 <oasis10702@gmail.com>
2020-03-27 18:06:27 +0900

    add problem17

---
+++ medium/17.letter-combinations-of-a-phone-number.js
@@ -0,0 +1,75 @@
+/*
+ * @lc app=leetcode id=17 lang=javascript
+ *
+ * [17] Letter Combinations of a Phone Number
+ *
+ * https://leetcode.com/problems/letter-combinations-of-a-phone-number/description/
+ *
+ * algorithms
+ * Medium (44.00%)
+ * Likes:    3301
+ * Dislikes: 375
+ * Total Accepted:    543.6K
+ * Total Submissions: 1.2M
+ * Testcase Example:  '"23"'
+ *
+ * Given a string containing digits from 2-9 inclusive, return all possible
+ * letter combinations that the number could represent.
+ *
+ * A mapping of digit to letters (just like on the telephone buttons) is given
+ * below. Note that 1 does not map to any letters.
+ *
+ *
+ *
+ * Example:
+ *
+ *
+ * Input: "23"
+ * Output: ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"].
+ *
+ *
+ * Note:
+ *
+ * Although the above answer is in lexicographical order, your answer could be
+ * in any order you want.
+ *
+ */
+
+// @lc code=start
+/**
+ * @param {string} digits
+ * @return {string[]}
+ */
+var letterCombinations = function(digits) {
+  if (!digits) return [];
+  const phone = {
+    2: ['a', 'b', 'c'],
+    3: ['d', 'e', 'f'],
+    4: ['g', 'h', 'i'],
+    5: ['j', 'k', 'l'],
+    6: ['m', 'n', 'o'],
+    7: ['p', 'q', 'r', 's'],
+    8: ['t', 'u', 'v'],
+    9: ['w', 'x', 'y', 'z']
+  };
+  const ans = [];
+  backtrack();
+
+  function backtrack(idx = 0, cur = '') {
+    if (idx === digits.length) {
+      ans.push(cur);
+      return;
+    }
+
+    const chars = phone[digits[idx]];
+    for (let i = 0; i < chars.length; i++) {
+      const tmp = cur;
+      cur += chars[i];
+      backtrack(idx + 1, cur);
+      cur = tmp;
+    }
+  }
+
+  return ans;
+};
+// @lc code=end
