161872ed26d6d02d7f7b134844baeafcbc1f1607
oasis10702 <oasis10702@gmail.com>
2020-03-29 22:42:39 +0900

    update problem119

--- easy/119.pascals-triangle-ii.js
+++ easy/119.pascals-triangle-ii.js
@@ -10,6 +10,12 @@
  * @return {number[]}
  */
 var getRow = function(rowIndex) {
-  // TODO
+  if (rowIndex === 0) return [1];
+  if (rowIndex === 1) return [1, 1];
+  if (rowIndex === 2) return [1, 2, 1];
+
+  for (let i = 3; i < rowIndex + 1; i++) {
+    // TODO
+  }
 };
 // @lc code=end
