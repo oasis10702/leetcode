256a1866050615937629fa7aecf7740f34727f05
oasis10702 <oasis10702@gmail.com>
2020-04-03 23:16:15 +0900

    update problem290

--- easy/290.word-pattern.js
+++ easy/290.word-pattern.js
@@ -14,22 +14,33 @@ var wordPattern = function(pattern, str) {
   str = str.split(' ');
   if (str.length !== pattern.length) return false;
   const tb = {};
-  let i = 0;
+  const tb2 = {};
 
-  for (let p of pattern) {
-    if (!tb[p]) {
-      if (Object.values(tb).indexOf(str[i]) !== -1) {
-        return false;
-      }
-      tb[p] = str[i];
-    } else {
-      if (tb[p] !== str[i]) {
-        return false;
-      }
-    }
-    i++;
+  for (let i = 0; i < pattern.length; i++) {
+    const v1 = pattern[i];
+    const v2 = str[i];
+    if (tb[v1] && tb[v1] !== v2) return false;
+    if (tb2[v2] && tb2[v2] !== v1) return false;
+    tb[v1] = v2;
+    tb2[v2] = v1;
   }
 
   return true;
+
+  //   for (let p of pattern) {
+  //     if (!tb[p]) {
+  //       if (Object.values(tb).indexOf(str[i]) !== -1) {
+  //         return false;
+  //       }
+  //       tb[p] = str[i];
+  //     } else {
+  //       if (tb[p] !== str[i]) {
+  //         return false;
+  //       }
+  //     }
+  //     i++;
+  //   }
+
+  //   return true;
 };
 // @lc code=end
