d151daba1e4d4bf9f92907ea431c671ac0191e7f
oasis10702 <oasis10702@gmail.com>
2020-04-04 23:22:02 +0900

    add problem136

---
+++ easy/136.single-number.js
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=136 lang=javascript
+ *
+ * [136] Single Number
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var singleNumber = function(nums) {
+  const map = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    if (!map[nums[i]]) {
+      map[nums[i]] = 1;
+    } else {
+      map[nums[i]]++;
+    }
+  }
+
+  for (let k in map) {
+    if (map[k] === 1) return k;
+  }
+};
+// @lc code=end
