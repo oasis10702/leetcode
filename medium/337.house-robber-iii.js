d545cb5c0f5536d573f1e6bbd4be15347b5cfba5
oasis10702 <oasis10702@gmail.com>
2020-04-10 03:47:24 +0900

    update problem337

--- medium/337.house-robber-iii.js
+++ medium/337.house-robber-iii.js
@@ -17,6 +17,7 @@
  * @return {number}
  */
 var rob = function(root) {
+  /* dfs solution
   if (!root) return 0;
 
   let money = root.val;
@@ -25,5 +26,25 @@ var rob = function(root) {
   if (root.right) money += rob(root.right.left) + rob(root.right.right);
 
   return Math.max(money, rob(root.left) + rob(root.right));
+  */
+
+  const result = robInternal(root);
+  // result[0] => no rob
+  // result[1] => rob
+  return Math.max(result[0], result[1]);
 };
+
+function robInternal(root) {
+  const result = [0, 0];
+  if (!root) return result;
+
+  const left = robInternal(root.left);
+  const right = robInternal(root.right);
+
+  result[0] = Math.max(left[0], left[1]) + Math.max(right[0], right[1]);
+  result[1] = left[0] + right[0] + root.val;
+
+  return result;
+}
+
 // @lc code=end
