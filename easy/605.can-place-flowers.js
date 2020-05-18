8fa6601cbc5a8831b4d28a214cc91ea2881615cd
oasis10702 <oasis10702@gmail.com>
2020-05-18 22:11:39 +0900

    add problem605

---
+++ easy/605.can-place-flowers.js
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=605 lang=javascript
+ *
+ * [605] Can Place Flowers
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} flowerbed
+ * @param {number} n
+ * @return {boolean}
+ */
+var canPlaceFlowers = function(flowerbed, n) {
+  let count = n;
+  if (count === 0) return true;
+
+  for (let i = 0; i < flowerbed.length; i++) {
+    if (flowerbed[i] === 0 && flowerbed[i - 1] !== 1 && flowerbed[i + 1] !== 1) {
+      flowerbed[i] = 1;
+      count--;
+    }
+
+    if (count === 0) {
+      return true;
+    }
+  }
+
+  return false;
+};
+// @lc code=end
