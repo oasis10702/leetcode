369ddb896d3de8341ba85bacc7ab528e86dd58d2
oasis10702 <oasis10702@gmail.com>
2020-05-26 12:53:48 +0900

    add problem704

---
+++ easy/704.binary-search.js
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=704 lang=javascript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var search = function(nums, target) {
+  let lo = 0;
+  let hi = nums.length - 1;
+
+  while (lo <= hi) {
+    const mid = Math.floor((lo + hi) / 2);
+    const n = nums[mid];
+
+    if (target > n) {
+      lo = mid + 1;
+    } else if (target < n) {
+      hi = mid - 1;
+    } else if (target === n) {
+      return mid;
+    }
+  }
+
+  return -1;
+};
+// @lc code=end
