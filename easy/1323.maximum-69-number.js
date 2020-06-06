9e3129b1335a8929f1ebf93c4b09ef7add8ecbe1
oasis10702 <oasis10702@gmail.com>
2020-06-06 17:42:33 +0900

    add problem1323

---
+++ easy/1323.maximum-69-number.js
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1323 lang=javascript
+ *
+ * [1323] Maximum 69 Number
+ */
+
+// @lc code=start
+/**
+ * @param {number} num
+ * @return {number}
+ */
+var maximum69Number = function(num) {
+  return num.toString().replace('6', '9');
+};
+// @lc code=end
